44d4aeb66a60c4b0b57cfc0e31fee86dcbdd3140
Alessandro Cabral <alessandro.afcabral222@gmail.com>
2026-03-11 23:02:09 -0300

    init exercicio 2 da aula 1

---
+++ aula01/src/escola/Aluno.java
@@ -0,0 +1,16 @@
+package escola;
+
+public class Aluno {
+//max(((P1*0.5+E1*0.2+E2*0.3+X+SUB*0.15)*0.5)+(max(P1*0.5+E1*0.2+E2*0.3+X+(SUB*0.15)-5.9,0)/(P1*0.5+E1*0.2+E2*0.3+X+(SUB*0.15)-5.9))API*0.5,EXF)
+
+    double P1 = 0;
+    double E1 = 0;
+    double E2 = 0;
+    double X = 0; // gincana
+    double SUB = 0;
+    double API = 0;
+    double EXF = 0;
+
+
+
+}
