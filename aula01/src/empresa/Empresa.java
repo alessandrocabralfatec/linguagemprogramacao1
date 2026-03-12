253ebcc3e972889845be802593da2e51e27aeaad
Alessandro Cabral <alessandro.afcabral222@gmail.com>
2026-03-11 22:50:05 -0300

    Feat exercicio Empresa

--- aula01/src/empresa/Empresa.java
+++ aula01/src/empresa/Empresa.java
@@ -1,6 +1,7 @@
 package empresa;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Empresa {
 
@@ -18,13 +19,21 @@ public class Empresa {
         return this.janeiro.add(this.fevereiro).add(this.marco);
     }
 
-    public BigDecimal MediaAritmetica(){
+    public BigDecimal mediaAritmetica(){
 
         BigDecimal quociente = new BigDecimal(3);
-        return this.somaTrimestral().divide(quociente) ;
+
+
+
+        return this.somaTrimestral().divide(quociente, 2, RoundingMode.HALF_UP);
     }
 
     public static void main(String[] args) {
-        System.out.println("teste");
+
+        Empresa trimestre = new Empresa(new BigDecimal(15000), new BigDecimal(23000), new BigDecimal(17000));
+
+        System.out.println("A receita do trimestre é de: R$" + trimestre.somaTrimestral());
+        System.out.println("A média é de: R$" + trimestre.mediaAritmetica());
+
     }
 }
