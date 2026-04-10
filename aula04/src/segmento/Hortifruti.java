556c1daafe05c003c58874f57e7248a14bc6d6c6
alessandro cabral <alessandro.afcabral222@gmail.com>
2026-04-09 22:28:22 -0300

    doc: mermaid teste

---
+++ aula04/src/segmento/Hortifruti.java
@@ -0,0 +1,110 @@
+package aula04.src.segmento;
+
+public class Hortifruti {
+
+
+    private String nome;
+    private float estoqueMaximo;
+    private float estoqueMinimo;
+    private float estoque;
+    private float preco;
+    private String unidade;
+
+//construtor
+    public Hortifruti (String nome,float preco, float estoque,float estoqueMinimo, float estoqueMaximo, String unidade){
+        this.nome = nome;
+        this.estoque = estoque;
+        this.preco=preco;
+        this.estoqueMinimo=estoqueMinimo;
+        this.estoqueMaximo=estoqueMaximo;
+        this.unidade = unidade;
+    }
+
+    public Hortifruti (String nome,float preco, String unidade){
+        this.nome = nome;
+        this.estoque = 0;
+        this.preco=preco;
+        this.estoqueMinimo=0;
+        this.estoqueMaximo=99999;
+        this.unidade = unidade;
+    }
+
+public void abastecer(float entradaKg){
+        float novoEstoque = this.estoque + entradaKg;
+
+        if (novoEstoque > this.estoqueMaximo){
+            System.out.println("Ta ficando louco? Vai apodrecer todo o estoque se voce comprar isso aqui");
+        return;
+        }
+this.estoque = this.estoque + entradaKg;
+    }
+
+    public void vender(float entradaKg){
+        float novoEstoque = this.estoque - entradaKg;
+
+        if (novoEstoque < 0){
+            System.out.println("Ta ficando louco? Você nao tem isso tudo de "+ this.nome + ". Voce so tem "+this.estoque+" KG");
+            return;
+        }
+        this.estoque =novoEstoque;
+    }
+
+    public void perda(float entradaKg){
+        float novoEstoque = this.estoque - entradaKg;
+
+        if (novoEstoque < 0){
+            System.out.println("Impossivel! Nao tem como jogar tudo isso de "+ this.nome + " fora. Voce so tem "+this.estoque+" KG no estoque");
+            return;
+        }
+
+        System.out.println("Parabéns! Você acaba de jogar "+entradaKg+" Kg de "+ this.nome + " na lata de lixo");
+
+        this.estoque =novoEstoque;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getEstoqueMaximo() {
+        return estoqueMaximo;
+    }
+
+    public void setEstoqueMaximo(float estoqueMaximo) {
+        this.estoqueMaximo = estoqueMaximo;
+    }
+
+    public float getEstoqueMinimo() {
+        return estoqueMinimo;
+    }
+
+    public void setEstoqueMinimo(float estoqueMinimo) {
+        this.estoqueMinimo = estoqueMinimo;
+    }
+
+    public float getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(float estoque) {
+        this.estoque = estoque;
+    }
+
+    public float getPrecoKg() {
+        return preco;
+    }
+
+    public void setPrecoKg(float precoKg) {
+        this.preco = precoKg;
+    }
+
+
+    public static void main(String[] args){
+        System.out.println("Olha a laranja! Laranja Laranja!");
+    }
+}
---
+++ aula04/src/segmento/README.md
@@ -0,0 +1,60 @@
+
+## Exercicio aula 4
+
+Para cada uma das imagens contidas nos slides seguintes, faça o que
+se pede nos itens seguintes.
+
+- a)  Faça os diagramas UML de três classes, cujos possíveis objetos
+estão exibidos nas imagens. Cada classe deve ter três atributos e
+três métodos, sem contar os métodos de acesso.
+- b) Implemente as classes definidas no item a em Java.
+- c) Construa uma classe principal, que deve conter o método main para
+instanciação de objetos e testes das classes codificadas no item b
+
+Ali temos 3 imagens: uma farmácia, uma feira e um salão de beleza.
+
+
+
+```mermaid
+classDiagram
+    Animal <|-- Duck
+    Animal <|-- Fish
+    Animal <|-- Zebra
+    Animal : +int age
+    Animal : +String gender
+    Animal: +isMammal()
+    Animal: +mate()
+    class Duck{
+      +String beakColor
+      +swim()
+      +quack()
+    }
+    class Fish{
+      -int sizeInFeet
+      -canEat()
+    }
+    class Zebra{
+      +bool is_wild
+      +run()
+    }
+
+```
+
+```mermaid
+classDiagram
+    Hortifruti : -String nome
+    Hortifruti : -float estoque
+    Hortifruti : -String unidade
+    Hortifruti: +abastecer(float entradaKg)
+    Hortifruti: +vender(float entradaKg)
+    Hortifruti: +perda(float entradaKg)
+   
+
+```
+
+    private String nome;
+    private float estoqueMaximo;
+    private float estoqueMinimo;
+    private float estoque;
+    private float preco;
+    private String unidade;
